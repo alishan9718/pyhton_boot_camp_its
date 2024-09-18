8609c65870ccf107c52087b65709b4667fb9ce9d
alishan <alishan9718@gmail.com>
2024-09-18 09:56:23 +0530

    update

---
+++ otpGenerator.py
@@ -0,0 +1,9 @@
+import random
+
+
+# otp generator using random function
+
+
+otp=random.randint(100000,999999)
+
+print("your OTP is :",otp)
--- variable.py
+++ variable.py
@@ -6,8 +6,19 @@
 
 
 #first day..
+#variable
 
+#int
+a=10
+b=20
+c=a+b
+print(c)
+#float
+x=1.10
 
+#sting
+y="hello world"
+ 
 print("My name is Alishan Ahmad")
 name=input("enter your name ")
 print(name)
